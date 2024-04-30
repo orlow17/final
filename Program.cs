17c8e421e4aa78def2ede1d8a1ccb1918e433441
Vladimir <wl.orlow_758@mail.ru>
2024-04-30 21:07:43 +0300

    added print

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
-﻿string[] FillArray()
+﻿string[] Array()
 {
-    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
-    string? enterSymbols = Console.ReadLine();
-    if (enterSymbols == null) { enterSymbols = ""; };
+    Console.WriteLine("Введите через пробел, по окончании ввода нажмите Enter: ");
+    string? Symbols = Console.ReadLine();
+    if (Symbols == null) { Symbols = ""; };
     char[] separators = new char[] { ',', ' ' };
-    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-    return workArray;
+    string[] workArr = Symbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return workArr;
+}
+string PrintArray(string[] workArray)
+{
+    string stringArray = "[";
+    for (int i = 0; i < workArray.Length; i++)
+    {
+        if (i == workArray.Length - 1)
+        {
+            stringArray += $"\"{workArray[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{workArray[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
 }
