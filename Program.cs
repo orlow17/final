19a7733d46f2897feca9d2f1ed82947d13ee895a
Vladimir <wl.orlow_758@mail.ru>
2024-04-30 21:04:25 +0300

    added array

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string[] FillArray()
+{
+    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return workArray;
+}
